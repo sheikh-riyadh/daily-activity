d26ab81e641b9434e62d3f1cd7b5cbc2207671d2
Sheikh Riyadh <sheikhriyad350883@gmail.com>
2022-09-29 12:32:03 +0600

    added new single item

---
+++ src/components/Item/Item.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Item.css'
+
+const Item = ({ item, addItemsHandler }) => {
+    const { img, activityName, description, time } = item;
+    return (
+        <div className='item-container'>
+            <img src={img} alt="ActivityImage" />
+            <h3>{activityName}</h3>
+            <p>{description.length < 150 ? description : description.slice(0, 150)}</p>
+            <h4>Time: {time}</h4>
+            <button onClick={() => addItemsHandler(time)}>Add to list</button>
+        </div>
+    );
+};
+
+export default Item;
